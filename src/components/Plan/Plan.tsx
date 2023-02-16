b7c2de2e79eb3ac0de7d5b38d94624bab52af9cb
Kasia <kajzerkatarzyna1@gmail.com>
2023-02-16 17:08:36 +0100

    Added styles of tables.

--- src/components/Plan/Plan.tsx
+++ src/components/Plan/Plan.tsx
@@ -4,6 +4,7 @@ import {Logo} from "../Logo/Logo";
 export const Plan = () => {
     return (
     <div className="plan">
+        <h2>Mój plan</h2>
         <Logo to="/exercises" text="Rozgrzewka"/>
         <Logo to="/exercises" text="Plan A"/>
         <Logo to="/exercises" text="Plan B"/>
