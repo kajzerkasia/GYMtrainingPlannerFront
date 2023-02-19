db091e04e458cdbf621472ba58d8f3957f6d6758
Kasia <kajzerkatarzyna1@gmail.com>
2023-02-19 13:16:42 +0100

    Changed names to Polish.

--- src/components/Plan/AddPlan.tsx
+++ src/components/Plan/AddPlan.tsx
@@ -6,8 +6,8 @@ export const AddPlan = () => {
     return (
         <div className="plan">
             <Logo to="/add-exercises" text="Rozgrzewka"/>
-            <Logo to="/add-exercises" text="Plan A"/>
-            <Logo to="/add-exercises" text="Plan B"/>
+            <Logo to="/add-exercises" text="Dzień A"/>
+            <Logo to="/add-exercises" text="Dzień B"/>
             <Logo to="/add-progression" text="Progresja"/>
         </div>
     )
