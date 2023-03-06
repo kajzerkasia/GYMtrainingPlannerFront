57a41ba3b74c144fef87729ad07a7e2433f43443
Kasia <kajzerkatarzyna1@gmail.com>
2023-03-06 20:24:42 +0100

    Only working with input table, trying to make update of inputs data in database and display after change fields of table inputs.

--- src/components/Table/TableFormInputs.tsx
+++ src/components/Table/TableFormInputs.tsx
@@ -3,9 +3,10 @@ import {TableHeader} from "./TableHeader";
 import {VscAdd, VscChromeClose} from "react-icons/vsc";
 import {TableBody} from "./TableBody";
 import './TableFormInputs.css';
+import {PartOfPlanEntity} from 'types';
 
 
-export const TableFormInputs = ({ savePartOfPlan, form, updateForm, partsList }: any) => {
+export const TableFormInputs = ({ savePartOfPlan, form, updateForm, partsList, updateTable, editTable }: PartOfPlanEntity[] | any) => {
 
     return (
         <>
@@ -20,7 +21,7 @@ export const TableFormInputs = ({ savePartOfPlan, form, updateForm, partsList }:
                             <input
                                 type="text"
                                 name="order"
-                                required
+                                // required
                                 maxLength={49}
                                 value={form.order}
                                 onChange={e => updateForm('order', e.target.value)}
@@ -30,7 +31,7 @@ export const TableFormInputs = ({ savePartOfPlan, form, updateForm, partsList }:
                             <input
                                 type="text"
                                 name="exercise"
-                                required
+                                // required
                                 maxLength={99}
                                 value={form.exercise}
                                 onChange={e => updateForm('exercise', e.target.value)}
@@ -86,7 +87,11 @@ export const TableFormInputs = ({ savePartOfPlan, form, updateForm, partsList }:
                             <button type="submit"><VscAdd className="icon"/></button>
                         </td>
                     </tr>
-                    <TableBody partsList={partsList}/>
+                    <TableBody
+                        partsList={partsList}
+                        updateTable={updateTable}
+                        editTable={editTable}
+                    />
                     </tbody>
                 </table>
             </form>
