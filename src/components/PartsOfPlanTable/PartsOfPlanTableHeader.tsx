3d4ed3b9fcfd16fa01bd249f560e2a3195e5157a
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-04 19:24:53 +0100

    Added passing "itemsList" prop to PartsOfPlanTableHeader to get to the plan name there

--- src/components/PartsOfPlanTable/PartsOfPlanTableHeader.tsx
+++ src/components/PartsOfPlanTable/PartsOfPlanTableHeader.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import {IconContext} from "react-icons";
 import {Link} from "react-router-dom";
 import {TbDotsVertical} from "react-icons/tb";
+import {PlanEntity} from 'types';
 
 interface PartsOfPlanTableHeaderProps {
     params: Record<string, string | undefined>;
+    itemsList: PlanEntity[];
 }
-const PartsOfPlanTableHeader = ({params}: PartsOfPlanTableHeaderProps) => {
+const PartsOfPlanTableHeader = ({params, itemsList}: PartsOfPlanTableHeaderProps) => {
+
+    const matchingItem = itemsList.find(item => item.slug === params.slug);
+    console.log(itemsList);
+    const firstItemName = matchingItem ? matchingItem.name : "Brak nazwy";
+
+
     return (
         <>
             <tr className="tr-add">
                 <td colSpan={3} className="training-plan">
-                    <h1 className="h1-plan">Nazwa planu treningowego (TODO)</h1>
+                    <h1 className="h1-plan">{firstItemName}</h1>
                 </td>
                 <td className="dots" colSpan={1}>
                     <IconContext.Provider value={{className: 'react-icons-dots'}}>
