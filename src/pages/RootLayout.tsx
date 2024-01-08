472faca8215bb86f672f65a33ff94fff2517413b
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-08 19:44:43 +0100

    Added useEffect hook in RootLayout.tsx

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MainNavigation from "../components/MainNavigation/MainNavigation";
-import {Outlet} from "react-router-dom";
+import {Outlet, useLoaderData, useSubmit} from "react-router-dom";
 import {useSelector} from "react-redux";
 import AppNotification from "../components/UI/AppNotification";
 import {RootState} from "../store";
+import {getTokenDuration} from "../helpers/auth";
 
 const RootLayout = () => {
     const notification = useSelector((state: RootState) => state.ui.notification);
+
+    const token = useLoaderData();
+    const submit = useSubmit();
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+
+        if (token === 'EXPIRED') {
+            submit(null, {action: '/logout', method: 'post'});
+            return;
+        }
+
+        const tokenDuration = getTokenDuration();
+        console.log(tokenDuration);
+
+        setTimeout(() => {
+            submit(null, {action: '/logout', method: 'post'});
+        }, tokenDuration);
+    }, [token, submit]);
 
     return (
         <>
