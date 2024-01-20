cbd7876aae5cb73cf8233ea6823613f358c4b914
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-20 20:55:20 +0100

    Moved some JSX code to another component.

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,16 +3,14 @@ import './Calendar.css'
 import {BasicCalendar} from "../BasicCalendar/BasicCalendar";
 import {Accordion} from "../Accordion/Accordion";
 import {DemoSign} from "../DemoSign/DemoSign";
-import {TbPlus, TbX, TbPencil} from "react-icons/tb";
-import {ADD_TRAINING_INSTRUCTION, DELETE_TRAINING_INSTRUCTION, EDIT_TRAINING_INSTRUCTION} from "../../constants/calendarInstruction";
-import CalendarInstructionList from "./CalendarInstructionList";
+import CalendarInstruction from "./CalendarInstruction";
 
 export const Calendar = () => {
 
     const [showInstructions, setShowInstructions] = useState(true);
 
     const toggleInstructions = () => {
-        setShowInstructions(!showInstructions);
+        setShowInstructions(prevShowInstructions => !prevShowInstructions);
     };
 
     return (
@@ -24,26 +22,9 @@ export const Calendar = () => {
                     toggleAccordion={toggleInstructions}
                 >
                     <DemoSign/>
-                    <div className="calendar-div">
-                        <CalendarInstructionList
-                            array={ADD_TRAINING_INSTRUCTION}
-                            title="Jak dodać trening do kalendarza?"
-                        >
-                            <TbPlus/>
-                        </CalendarInstructionList>
-                        <CalendarInstructionList
-                            array={EDIT_TRAINING_INSTRUCTION}
-                            title="Jak edytować dodany trening w kalendarzu?"
-                        >
-                            <TbPencil/>
-                        </CalendarInstructionList>
-                        <CalendarInstructionList
-                            array={DELETE_TRAINING_INSTRUCTION}
-                            title="Jak usunąć dany trening z kalendarza?"
-                        >
-                            <TbX/>
-                        </CalendarInstructionList>
-                    </div>
+                    <CalendarInstruction
+                        toggleAccordion={toggleInstructions}
+                    />
                 </Accordion>
                 <div style={{height: "730px"}} className="div-calendar">
                     <BasicCalendar/>
