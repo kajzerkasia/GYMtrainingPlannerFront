beae0babb0a1a0031dce16b6cce35c7d8f095bdf
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-22 13:28:49 +0100

    Added AppThunk type.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {Action, configureStore, ThunkAction} from "@reduxjs/toolkit";
 import itemsSlice from "./features/items/items-slice";
 import uiSlice from "./features/ui/ui-slice";
 import calendarSlice from "./features/calendar/calendar-slice";
@@ -15,3 +15,4 @@ export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
