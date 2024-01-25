9bc35ac6857f88f2762d16fc7e02702a05a77e04
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-25 21:52:32 +0100

    Removed props, added Modal.

--- src/components/PartsOfPlanTable/AddPartsOfPlanElements.tsx
+++ src/components/PartsOfPlanTable/AddPartsOfPlanElements.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import {IconContext} from "react-icons";
-import {Link} from "react-router-dom";
-import {TbQuestionMark, TbStairsUp} from "react-icons/tb";
+import {Link, useParams} from "react-router-dom";
+import {TbAlertTriangle, TbQuestionMark, TbStairsUp} from "react-icons/tb";
 import {PartsOfPlanForm} from "./PartsOfPlanForm";
 import {Status} from 'types';
-import {PartOfPlanEntity} from 'types';
+import {useDispatch} from "react-redux";
+import {sendPartsOfPlanData} from "../../store/actions/parts-of-plan/sending/sending-action";
+import {UseModal} from "../../hooks/useModal";
+import UseModals from "../../hooks/useModals";
+import Modal from "../Modal/Modal";
+import {textInformation} from "../../constants/partsOfPlanTableTexts";
 
-interface AddPartsOfPlanElementsProps {
-    addPartOfPlan: (values: PartOfPlanEntity) => void;
-    params: Record<string, string | undefined>;
-}
+const AddPartsOfPlanElements = () => {
 
-const AddPartsOfPlanElements = ({addPartOfPlan, params}: AddPartsOfPlanElementsProps) => {
+    const dispatch = useDispatch();
+
+    const {setDemoModalIsOpen, setInformationModalIsOpen} = UseModal();
+
+    const params = useParams();
+
+    const {
+        isValidationModalOpen,
+        openValidationModal,
+        closeValidationModal,
+    } = UseModals();
+
     return (
         <tr>
+            <Modal
+                open={isValidationModalOpen}
+                onClose={closeValidationModal}
+                onCancel={closeValidationModal}
+                modalText={textInformation}
+                cancelText="Rozumiem"
+                icon={TbAlertTriangle}
+            />
             <td className="question-td">
                 <IconContext.Provider value={{className: 'react-icons'}}>
                     <Link to="/instruction"><TbQuestionMark/></Link>
@@ -24,8 +45,12 @@ const AddPartsOfPlanElements = ({addPartOfPlan, params}: AddPartsOfPlanElementsP
                     name: '',
                 }}
                 onSubmit={async (values, reset) => {
-                    addPartOfPlan(values);
-                    reset();
+                    if (values.name === '') {
+                        openValidationModal();
+                    } else {
+                        dispatch(sendPartsOfPlanData(values, setDemoModalIsOpen, setInformationModalIsOpen, params) as any);
+                        reset();
+                    }
                 }}
                 actionType={Status.Add}
             />
