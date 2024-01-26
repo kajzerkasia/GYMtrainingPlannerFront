36718b4620d8fe73d2e5c289330ee6909f509fd1
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-26 15:28:07 +0100

    Added onSubmit prop.

--- src/pages/Table.tsx
+++ src/pages/Table.tsx
@@ -5,6 +5,7 @@ import AddTableElements from "../components/Table/AddTableElements";
 import TableHeader from "../components/Table/TableHeader";
 import React from "react";
 import RedirectLink from "../components/RedirectLink";
+import {PartOfPlanEntity} from 'types';
 
 interface Link {
     icon: React.ComponentType<any>;
@@ -13,8 +14,10 @@ interface Link {
 
 interface TableProps {
     links: Link[];
+    onSubmit: (values: PartOfPlanEntity, reset: () => void) => void | Promise<void>;
 }
-export const Table = ({links}: TableProps) => {
+
+export const Table = ({links, onSubmit}: TableProps) => {
 
     const renderLink = (link: Link) => (
         <RedirectLink
@@ -27,21 +30,23 @@ export const Table = ({links}: TableProps) => {
         <div className="parts-wrapper">
             <div className="main-plan">
                 <DemoSign/>
-                        <table className="main-table">
-                            <thead>
-                            <TableHeader>
-                                {renderLink(links[0])}
-                            </TableHeader>
-                            </thead>
-                            <tbody>
-                            <AddTableElements>
-                                {renderLink(links[1])}
-                            </AddTableElements>
-                            <TableElements>
-                                {renderLink(links[2])}
-                            </TableElements>
-                            </tbody>
-                        </table>
+                <table className="main-table">
+                    <thead>
+                    <TableHeader>
+                        {renderLink(links[0])}
+                    </TableHeader>
+                    </thead>
+                    <tbody>
+                    <AddTableElements
+                        handleSubmit={onSubmit}
+                    >
+                        {renderLink(links[1])}
+                    </AddTableElements>
+                    <TableElements>
+                        {renderLink(links[2])}
+                    </TableElements>
+                    </tbody>
+                </table>
                 <button className="btn-back-exercises" onClick={() => window.history.back()}>
                     Powrót
                 </button>
