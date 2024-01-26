692acff14c9e8977673c12d4c8bfd8ce050c8e01
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-26 15:41:23 +0100

    Added props.

--- src/components/Table/AddTableElements.tsx
+++ src/components/Table/AddTableElements.tsx
@@ -1,28 +1,25 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import {IconContext} from "react-icons";
-import {Link, useParams} from "react-router-dom";
-import {TbAlertTriangle, TbQuestionMark, TbStairsUp} from "react-icons/tb";
-import {PartsOfPlanForm} from "./PartsOfPlanForm";
+import {Link} from "react-router-dom";
+import {TbAlertTriangle, TbQuestionMark} from "react-icons/tb";
+import {TableForm} from "./TableForm";
 import {Status} from 'types';
-import {useDispatch} from "react-redux";
-import {sendPartsOfPlanData} from "../../store/actions/parts-of-plan/sending/sending-action";
-import {UseModal} from "../../hooks/useModal";
 import UseModals from "../../hooks/useModals";
 import Modal from "../Modal/Modal";
 import {textInformation} from "../../constants/partsOfPlanTableTexts";
+import {PartOfPlanEntity} from 'types';
 
-const AddPartsOfPlanElements = () => {
+interface AddTableElementsProps {
+    children: ReactNode;
+    handleSubmit: (values: PartOfPlanEntity, reset: () => void) => void | Promise<void>;
+}
 
-    const dispatch = useDispatch();
-
-    const {setDemoModalIsOpen, setInformationModalIsOpen} = UseModal();
-
-    const params = useParams();
+const AddTableElements = ({children, handleSubmit}: AddTableElementsProps) => {
 
     const {
         isValidationModalOpen,
-        openValidationModal,
         closeValidationModal,
+        openValidationModal,
     } = UseModals();
 
     return (
@@ -40,27 +37,26 @@ const AddPartsOfPlanElements = () => {
                     <Link to="/instruction"><TbQuestionMark/></Link>
                 </IconContext.Provider>
             </td>
-            <PartsOfPlanForm
+            <TableForm
                 initialValues={{
                     name: '',
                 }}
                 onSubmit={async (values, reset) => {
-                    if (values.name === '') {
+                    if (!values.name) {
                         openValidationModal();
                     } else {
-                        dispatch(sendPartsOfPlanData(values, setDemoModalIsOpen, setInformationModalIsOpen, params) as any);
-                        reset();
+                        handleSubmit(values, reset);
                     }
                 }}
                 actionType={Status.Add}
             />
             <td className="td-progression-rules">
                 <IconContext.Provider value={{className: 'react-icons-progression'}}>
-                    <Link to={`/rules/${params.slug}`}><TbStairsUp/></Link>
+                    {children}
                 </IconContext.Provider>
             </td>
         </tr>
     );
 };
 
-export default AddPartsOfPlanElements;
+export default AddTableElements;
