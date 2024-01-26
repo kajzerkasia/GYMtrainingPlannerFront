96a4dc51886048d5b6d5a1208ab9301c2fe15558
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-26 21:53:32 +0100

    Added useExercisesActions.ts hook

---
+++ src/hooks/useExercisesActions.ts
@@ -0,0 +1,39 @@
+import {useDispatch} from "react-redux";
+import {UseModal} from "./useModal";
+import {useParams} from "react-router-dom";
+import {ExerciseEntity} from 'types';
+import {addExercise} from "../store/actions/exercises/sending/sending-action";
+import {itemsActions} from "../store/features/items/items-slice";
+import {editExercise} from "../store/actions/exercises/updating/updating-action";
+import {deleteExercise} from "../store/actions/exercises/deleting/deleting-action";
+
+const UseExercisesActions = () => {
+
+    const dispatch = useDispatch();
+
+    const {setDemoModalIsOpen, setInformationModalIsOpen, closeDemoModal} = UseModal();
+
+    const params = useParams();
+
+    const handleSubmit = (values: ExerciseEntity, reset: () => void) => {
+        dispatch(addExercise(values, setDemoModalIsOpen, setInformationModalIsOpen, params) as any);
+        reset();
+    }
+
+    const handleUpdate = (values: ExerciseEntity, reset: () => void) => {
+        dispatch(editExercise(values, setDemoModalIsOpen, setInformationModalIsOpen) as any);
+        dispatch(itemsActions.updateItem(values));
+    }
+
+    const handleDelete = () => {
+        dispatch(deleteExercise(closeDemoModal) as any);
+    }
+
+    return {
+        handleSubmit,
+        handleUpdate,
+        handleDelete,
+    }
+};
+
+export default UseExercisesActions;
