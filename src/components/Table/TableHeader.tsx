0a75aab6cb860572c9813356a7b6059d741b9c63
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-26 23:11:04 +0100

    Removed content.

--- src/components/Table/TableHeader.tsx
+++ src/components/Table/TableHeader.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import {IconContext} from "react-icons";
 const TableHeader = ({children}: any) => {
 
     return (
         <>
-            <tr className="tr-add">
-                <td colSpan={3} className="training-plan">
-                    <h1 className="h1-plan">Nazwa planu/części planu-TODO</h1>
-                </td>
-                <td className="dots" colSpan={1}>
-                    <IconContext.Provider value={{className: 'react-icons-dots'}}>
-                        {children}
-                    </IconContext.Provider>
-                </td>
-            </tr>
+            {children}
         </>
     );
 };
