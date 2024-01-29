06a235a25655bf150f80056ed4aaf87183c7a9b2
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-29 12:29:32 +0100

    Brought back here functions, state and input.

--- src/components/Table/TableForm.tsx
+++ src/components/Table/TableForm.tsx
@@ -1,30 +1,51 @@
-import React from 'react';
-import {PartOfPlanEntity, Status} from 'types';
+import React, {useState} from 'react';
+import {PartOfPlanEntity, Status, ExerciseEntity, PlanEntity} from 'types';
 import {TbPlus, TbCheck} from "react-icons/tb";
 import {IconContext} from "react-icons";
 import '../../pages/Table.css';
-import {useDispatch} from "react-redux";
-import {itemsActions} from "../../store/features/items/items-slice";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store";
 
 export type TableFormProps = {
-    initialValues: any;
-    onSubmit: (values: PartOfPlanEntity, reset: () => void) => void | Promise<void>;
+    onSubmit: (values: PartOfPlanEntity | ExerciseEntity | PlanEntity, reset: () => void) => void | Promise<void>;
     actionType: Status;
     children?: any;
     values?: any;
+    initialValues: any;
 };
 
-export const TableForm = ({initialValues, onSubmit, actionType, children, values}: TableFormProps) => {
-    const dispatch = useDispatch();
+export const TableForm = ({onSubmit, actionType, initialValues}: TableFormProps) => {
 
+    const [values, setValues] = useState<PartOfPlanEntity>(() => initialValues);
 
-    const reset = () => {
-        dispatch(itemsActions.setItemsList(initialValues));
-    }
+    const {isEdited} = useSelector((state: RootState) => state.items);
+
+    const reset: () => void = () => {
+        setValues(initialValues);
+    };
+
+    const handleChange: (field: keyof PartOfPlanEntity, value: string) => void = (field, value) => {
+        setValues(localValues => ({
+            ...localValues,
+            [field]: value
+        }));
+    };
+
+    // @TODO: render dynamic list of inputs depend on which table/entity
 
     return (
         <>
-            {children}
+            <td className="input-part-add">
+                <input
+                    placeholder="Podaj nazwę części planu, którą chcesz dodać"
+                    className={isEdited ? 'edited-input' : 'input-part'}
+                    type="text"
+                    name="name"
+                    required
+                    value={values.name}
+                    onChange={(event) => handleChange('name', event.target.value)}
+                />
+            </td>
             <td>
                 <IconContext.Provider value={{className: 'react-icons'}}>
                     <button type='button' onClick={() => onSubmit(values, reset)}>{actionType === Status.Add ? <TbPlus/> : <TbCheck/>}</button>
