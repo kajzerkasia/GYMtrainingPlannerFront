7240061307ec2d219da5f09eb76db8231f3d37c9
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-30 10:28:22 +0100

    Removed unnecessary code.

--- src/store/actions/exercises/fetching/fetching-action.ts
+++ src/store/actions/exercises/fetching/fetching-action.ts
@@ -2,7 +2,6 @@ import {AppDispatch, RootState} from "../../../index";
 import {itemsActions} from "../../../features/items/items-slice";
 import {apiUrl} from "../../../../config/api";
 import {uiActions} from "../../../features/ui/ui-slice";
-import {PlanEntity} from 'types';
 
 export const fetchExercises = (params: Record<string, string | undefined>) => {
     return async (dispatch: AppDispatch, getState: () => RootState) => {
@@ -29,16 +28,10 @@ export const fetchExercises = (params: Record<string, string | undefined>) => {
                         message: 'Wystąpił błąd podczas próby pobrania danych.'
                     }));
                 } else {
-                    const planInfoResponse = await fetch(`${apiUrl}/api/add-plan/list`);
-                    const plans = await planInfoResponse.json();
-                    const foundPlan = plans.find((plan: Partial<PlanEntity>) => plan.id === planPart[0].planId);
-
                     const exercisesResponse = await fetch(`${apiUrl}/api/add-exercise/exercises?partId=${planPart[0].id}`);
                     const exercises = await exercisesResponse.json();
 
                     dispatch(itemsActions.setItemsList(exercises));
-                    dispatch(itemsActions.setPartName(planPart[0].name));
-                    dispatch(itemsActions.setPlanInfo(foundPlan as PlanEntity));
                     dispatch(itemsActions.setIsLoading(false));
                 }
             }
