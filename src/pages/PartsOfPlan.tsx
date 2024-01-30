f34e27866bb67e44a55e246c9ef13173ceb454a3
Kasia <kajzerkatarzyna1@gmail.com>
2024-01-30 10:27:48 +0100

    Added PartsOfPlanTableHead to PartsOfPlan.tsx

--- src/pages/PartsOfPlan.tsx
+++ src/pages/PartsOfPlan.tsx
@@ -1,14 +1,15 @@
 import React, {useEffect} from 'react';
 import {Table} from "./Table";
 import {useParams} from "react-router-dom";
-import {TbDotsVertical, TbStairsUp} from "react-icons/tb";
+import {TbStairsUp} from "react-icons/tb";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchPartsOfPlanData} from "../store/actions/parts-of-plan/fetching/fetching-action";
 import UsePartsOfPlanActions from "../hooks/usePartsOfPlanActions";
 import {PartOfPlanEntity} from 'types';
-import {IconContext} from "react-icons";
-import RedirectLink from "../components/RedirectLink";
 import {RootState} from "../store";
+import {DemoSign} from "../components/DemoSign/DemoSign";
+import BackButton from "../components/BackButton/BackButton";
+import PartsOfPlanTableHead from "../components/Table/PartsOfPlanTableHead";
 
 const PartsOfPlan = () => {
 
@@ -21,55 +22,40 @@ const PartsOfPlan = () => {
         }
     }, [dispatch, params]);
 
+    const {itemsList} = useSelector((state: RootState) => state.items);
+
     const PARTS_OF_PLAN_LINKS = [
-        {
-            icon: TbDotsVertical,
-            path: `/details/${params.slug}`,
-        },
         {
             icon: TbStairsUp,
             path: `/rules/${params.slug}`,
-        },
+        }
     ]
 
-    const firstLinkPart = 'exercises';
-
-    const {itemsList} = useSelector((state: RootState) => state.items);
-
-
     const {handleSubmit, handleUpdate, handleDelete} = UsePartsOfPlanActions();
-
-    const tableHeader = (
-        <tr className="tr-add">
-            <td colSpan={3} className="training-plan">
-                <h1 className="h1-plan">Nazwa planu/części planu-TODO</h1>
-            </td>
-            <td className="dots" colSpan={1}>
-                <IconContext.Provider value={{className: 'react-icons-dots'}}>
-                    <RedirectLink
-                        icon={React.createElement(TbDotsVertical)}
-                        path={`/rules/${params.slug}`}
-                    />
-                </IconContext.Provider>
-            </td>
-        </tr>
-    )
 
     const availableFields = itemsList && itemsList.length > 0
         ? ['name']
         : [];
 
     return (
-        <Table
-            links={PARTS_OF_PLAN_LINKS}
-            onSubmit={async (values, reset) => handleSubmit((values as unknown as PartOfPlanEntity), reset)}
-            onUpdate={async (values, reset) => handleUpdate((values as unknown as PartOfPlanEntity), reset)}
-            onDelete={handleDelete}
-            firstLinkPath={firstLinkPart}
-            tableHeader={tableHeader}
-            availableFields={availableFields}
-        />
-    );
+        <div className="parts-wrapper">
+            <div className="main-plan">
+                <DemoSign/>
+                <table className="main-table">
+                    <PartsOfPlanTableHead/>
+                    <Table
+                        links={PARTS_OF_PLAN_LINKS}
+                        onSubmit={async (values, reset) => handleSubmit((values as unknown as PartOfPlanEntity), reset)}
+                        onUpdate={async (values, reset) => handleUpdate((values as unknown as PartOfPlanEntity), reset)}
+                        onDelete={handleDelete}
+                        availableFields={availableFields}
+                    />
+                </table>
+                <BackButton/>
+            </div>
+        </div>
+    )
+        ;
 };
 
 export default PartsOfPlan;
