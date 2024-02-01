8becad78dc2b9aa74c4538678d09e7c0fc8aa6ac
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-01 19:35:30 +0100

    Added fetching action for Plans.

---
+++ src/store/actions/plans-list/fetching/fetching-action.ts
@@ -0,0 +1,35 @@
+import {AppDispatch, RootState} from "../../../index";
+import {itemsActions} from "../../../features/items/items-slice";
+import {apiUrl} from "../../../../config/api";
+import {uiActions} from "../../../features/ui/ui-slice";
+
+export const fetchPlansData = () => {
+    return async (dispatch: AppDispatch, getState: () => RootState) => {
+        dispatch(itemsActions.setIsLoading(true));
+
+        try {
+            const plansResponse = await fetch(`${apiUrl}/api/add-plan/list`);
+            const plans = await plansResponse.json();
+            console.log(plans);
+            if (!plans || plans.length === 0) {
+                console.log('Brak planów.');
+                dispatch(uiActions.showNotification({
+                    status: 'error',
+                    title: 'Błąd!',
+                    message: 'Wystąpił błąd podczas próby pobrania danych.'
+                }));
+            } else {
+                dispatch(itemsActions.setItemsList(plans));
+                dispatch(itemsActions.setIsLoading(false));
+            }
+        } catch (error) {
+            console.error('Wystąpił błąd podczas próby pobrania danych:', error);
+            dispatch(itemsActions.setIsLoading(false));
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Wystąpił błąd podczas próby pobrania danych.'
+            }));
+        }
+    };
+};
