a996dfcfb0fb2f149fd338ea4761c5ce49a011cf
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-02 17:13:04 +0100

    Added PlanDetailsTableHead.tsx

---
+++ src/components/Table/PlanDetailsTableHead.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PlanDetailsTableHead = () => {
+    return (
+        <thead>
+        <tr>
+            <th>Długość cyklu</th>
+            <th>Częstotliwość</th>
+            <th>Rozkład</th>
+        </tr>
+        </thead>
+    );
+};
+
+export default PlanDetailsTableHead;
