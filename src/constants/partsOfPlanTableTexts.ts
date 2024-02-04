faed7ebc62b4478c1bd2f533e9dfda4ee68d46f1
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-04 17:51:55 +0100

    Removed partsOfPlanTableTexts.ts

--- src/constants/partsOfPlanTableTexts.ts
+++
@@ -1,3 +0,0 @@
-export const text = 'Czy na pewno chcesz usunąć tę część planu? Spowoduje to także usunięcie wszystkich ćwiczeń przypisanych do tej części planu';
-
-export const textInformation = 'Należy podać nazwę części planu!'
