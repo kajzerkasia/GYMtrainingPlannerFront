ad43873f65096bd2d9e015f6afe6714675ef78b8
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-04 19:55:40 +0100

    Adjusted fetching-action.ts to the backend changes.

--- src/store/actions/plans-list/fetching-action.ts
+++ src/store/actions/plans-list/fetching-action.ts
@@ -3,24 +3,46 @@ import {itemsActions} from "../../features/items/items-slice";
 import {apiUrl} from "../../../config/api";
 import {uiActions} from "../../features/ui/ui-slice";
 
-export const fetchPlansData = () => {
+export const fetchPlansData = (
+    params: Record<string, string | undefined>
+) => {
     return async (dispatch: AppDispatch, getState: () => RootState) => {
         dispatch(itemsActions.setIsLoading(true));
 
         try {
-            const plansResponse = await fetch(`${apiUrl}/api/add-plan/list`);
-            const plans = await plansResponse.json();
-            console.log(plans);
-            if (!plans || plans.length === 0) {
-                console.log('Brak planów.');
+            const usersResponse = await fetch(
+                `${apiUrl}/api/auth-user/users?slug=${params.slug}`,
+                {
+                    method: 'GET',
+                }
+            );
+
+            const users = await usersResponse.json();
+
+            if (!users || users.length === 0) {
+                console.log('Brak użytkowników.');
                 dispatch(uiActions.showNotification({
                     status: 'error',
                     title: 'Błąd!',
                     message: 'Wystąpił błąd podczas próby pobrania danych.'
-                }));
+                }))
             } else {
-                dispatch(itemsActions.setItemsList(plans));
-                dispatch(itemsActions.setIsLoading(false));
+                dispatch(itemsActions.setItemsList(users));
+
+                const plansResponse = await fetch(`${apiUrl}/api/add-plan/list?userId=${users[0].id}`);
+                const plans = await plansResponse.json();
+
+                if (!plans) {
+                    dispatch(uiActions.showNotification({
+                        status: 'error',
+                        title: 'Błąd!',
+                        message: 'Wystąpił błąd podczas próby pobrania danych.'
+                    }))
+                    return Promise.reject('Brak planów.');
+                } else {
+                    dispatch(itemsActions.setItemsList(plans));
+                    dispatch(itemsActions.setIsLoading(false));
+                }
             }
         } catch (error) {
             console.error('Wystąpił błąd podczas próby pobrania danych:', error);
