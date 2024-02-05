4b4db5d9b938a4072f93d0756791fdb41827425d
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-05 15:45:28 +0100

    Corrected text.

--- src/components/SuspenseFallback/SuspenseFallback.tsx
+++ src/components/SuspenseFallback/SuspenseFallback.tsx
@@ -4,8 +4,8 @@ import {MoonLoader} from "react-spinners";
 const SuspenseFallback = () => {
     return (
         <tr className="suspense-tr">
-            <td className="suspense-td">Loading...
-                <div className="div_loading">Ładowanie częsci planu..</div>
+            <td className="suspense-td">
+                <div className="div_loading">Ładowanie...</div>
                 <MoonLoader speedMultiplier={0.5} color="#9fc3f870"/>
             </td>
         </tr>
