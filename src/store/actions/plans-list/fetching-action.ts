64872b7a6451a058c45ec96e8bc0fe5d440b3797
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-07 23:44:35 +0100

    Removed unnecessary fetch.

--- src/store/actions/plans-list/fetching-action.ts
+++ src/store/actions/plans-list/fetching-action.ts
@@ -10,26 +10,7 @@ export const fetchPlansData = (
         dispatch(itemsActions.setIsLoading(true));
 
         try {
-            const usersResponse = await fetch(
-                `${apiUrl}/api/auth-user/users?id=${params.userId}`,
-                {
-                    method: 'GET',
-                }
-            );
-
-            const users = await usersResponse.json();
-
-            if (!users || users.length === 0) {
-                console.log('Brak użytkowników.');
-                dispatch(uiActions.showNotification({
-                    status: 'error',
-                    title: 'Błąd!',
-                    message: 'Wystąpił błąd podczas próby pobrania danych.'
-                }))
-            } else {
-                dispatch(itemsActions.setItemsList(users));
-
-                const plansResponse = await fetch(`${apiUrl}/api/add-plan/list?userId=${users[0].id}`);
+                const plansResponse = await fetch(`${apiUrl}/api/add-plan/list?userId=${params.userId}`);
                 const plans = await plansResponse.json();
 
                 if (!plans) {
@@ -44,7 +25,7 @@ export const fetchPlansData = (
                     dispatch(itemsActions.setIsLoading(false));
                 }
             }
-        } catch (error) {
+         catch (error) {
             console.error('Wystąpił błąd podczas próby pobrania danych:', error);
             dispatch(itemsActions.setIsLoading(false));
             dispatch(uiActions.showNotification({
