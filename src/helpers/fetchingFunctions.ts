bea415650be74a74a1774dff985c1d61fd05f1be
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-08 22:30:44 +0100

    Removed fetchTrainingPlans function.

--- src/helpers/fetchingFunctions.ts
+++ src/helpers/fetchingFunctions.ts
@@ -1,24 +1,5 @@
 import {apiUrl} from "../config/api";
-import {PlanEntity, PartOfPlanEntity} from 'types';
-
-export const fetchTrainingPlans = async (): Promise<PlanEntity[]> => {
-    try {
-        const response = await fetch(`${apiUrl}/api/add-plan/list`);
-        const plans = await response.json();
-
-        const plansWithIdsAsString = plans.map((plan: PlanEntity) => ({
-            ...plan,
-            id: String(plan.id),
-        }));
-
-        return plansWithIdsAsString;
-    } catch (error) {
-        if (error instanceof Response && error.status === 403) {
-            throw new Error('Permission denied: Unable to fetch training plans (403 Forbidden).');
-        }
-        throw new Error('Failed to fetch training plans.');
-    }
-};
+import {PartOfPlanEntity} from 'types';
 
 export const fetchPlanParts = async (planId: string): Promise<PartOfPlanEntity[]> => {
     try {
