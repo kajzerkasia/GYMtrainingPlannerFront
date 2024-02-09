7318441ce0d2d43b684e5825e5677dd035d6d1c6
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-09 13:56:18 +0100

    Replaced CSS file with .module.css file

--- src/components/MainNavigation/MainNavigation.tsx
+++ src/components/MainNavigation/MainNavigation.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect} from 'react';
 import {Form, Link, NavLink, useRouteLoaderData} from "react-router-dom";
-import './MainNavigation.css';
 import Logo from "../Logo/Logo";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchUsers} from "../../store/actions/users/fetching-action";
 import {RootState} from "../../store";
 import {UserEntity} from 'types';
+import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
     const token: any = useRouteLoaderData('root');
@@ -28,62 +28,60 @@ const MainNavigation = () => {
     }
 
     return (
-        <header className="header">
+        <header className={classes.header}>
             <Link to="/">
                 <Logo/>
             </Link>
             <nav>
-                <ul className="list">
-                    <>
+                <ul>
+                    <li>
+                        <NavLink to='/' className={({isActive}) =>
+                            isActive ? `${classes.active}` : undefined
+                        }>
+                            <button>
+                                Strona główna
+                            </button>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/list/${userId}`} className={({isActive}) =>
+                            isActive ? `${classes.active}` : undefined
+                        }>
+                            <button onClick={checkIfAuth}>
+                                Plany treningowe
+                            </button>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/calendar/${userId}`} className={({isActive}) =>
+                            isActive ? `${classes.active}` : undefined
+                        }>
+                            <button onClick={checkIfAuth}>
+                                Kalendarz
+                            </button>
+                        </NavLink>
+                    </li>
+                    {!token && (
                         <li>
-                            <NavLink to='/' className={({isActive}) =>
-                                isActive ? "active" : undefined
-                            }>
+                            <NavLink
+                                to="/auth?mode=login"
+                                className={({isActive}) =>
+                                    isActive ? `${classes.active}` : undefined
+                                }
+                            >
                                 <button>
-                                    Strona główna
+                                    Logowanie
                                 </button>
                             </NavLink>
                         </li>
+                    )}
+                    {token && (
                         <li>
-                            <NavLink to={`/list/${userId}`} className={({isActive}) =>
-                                isActive ? "active plans" : undefined
-                            }>
-                                <button onClick={checkIfAuth}>
-                                    Plany treningowe
-                                </button>
-                            </NavLink>
+                            <Form action="/logout" method="post">
+                                <button>Wyloguj się</button>
+                            </Form>
                         </li>
-                        <li>
-                            <NavLink to={`/calendar/${userId}`} className={({isActive}) =>
-                                isActive ? "active" : undefined
-                            }>
-                                <button onClick={checkIfAuth}>
-                                    Kalendarz
-                                </button>
-                            </NavLink>
-                        </li>
-                        {!token && (
-                            <li>
-                                <NavLink
-                                    to="/auth?mode=login"
-                                    className={({isActive}) =>
-                                        isActive ? "active" : undefined
-                                    }
-                                >
-                                    <button>
-                                        Logowanie
-                                    </button>
-                                </NavLink>
-                            </li>
-                        )}
-                        {token && (
-                            <li>
-                                <Form action="/logout" method="post">
-                                    <button>Wyloguj się</button>
-                                </Form>
-                            </li>
-                        )}
-                    </>
+                    )}
                 </ul>
             </nav>
         </header>
