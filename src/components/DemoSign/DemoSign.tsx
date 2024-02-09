866d724e3c1aad396833835767a2d10645af0a6d
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-09 13:41:52 +0100

    Changed CSS file to .module.css file, updated classNames

--- src/components/DemoSign/DemoSign.tsx
+++ src/components/DemoSign/DemoSign.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { isDemoEnabled } from '../../helpers/env';
-import './DemoSign.css';
 import { IconContext } from 'react-icons';
 import { TbAlertTriangle } from 'react-icons/tb';
+import classes from './DemoSign.module.css';
 
 export const DemoSign = () => {
     const [showDemoText, setShowDemoText] = useState(true);
@@ -14,14 +14,17 @@ export const DemoSign = () => {
     return (
         <>
             {isDemoEnabled() && (
-                <div className="div-demo">
-                    <IconContext.Provider value={{ className: 'react-triangle-icon' }}>
-                        <button className="demo-button" onClick={handleDemoButtonClick}>
+                <div className={classes.div_demo}>
+                    <IconContext.Provider value={{ className: `${classes.react_warning_icon}` }}>
+                        <button
+                            className={classes.demo_button}
+                            onClick={handleDemoButtonClick}
+                        >
                             <TbAlertTriangle/>
                             Wersja demo
                         </button>
                     </IconContext.Provider>
-                    <div className={`demo-text ${showDemoText ? 'show' : ''}`}>
+                    <div className={`${classes.demo_text} ${showDemoText ? `${classes.show}` : ''}`}>
                         - brak możliwości dodawania, edytowania oraz usuwania wybranych elementów
                     </div>
                 </div>
