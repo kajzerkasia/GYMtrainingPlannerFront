2eee7b019babafe58b9077aa2bb3caecdc3f428a
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-09 13:52:51 +0100

    Replaced CSS file with .module.css file, added new className.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {IconContext} from "react-icons";
 import {TbHeartbeat} from "react-icons/tb";
-import './Logo.css';
+import classes from './Logo.module.css';
 
 const Logo = () => {
     return (
-            <IconContext.Provider value={{className: 'react-main-icon'}}>
-                <h1 className="main-h1"><TbHeartbeat/> Gym Training Planner</h1>
+            <IconContext.Provider value={{className: `${classes.react_logo_icon}`}}>
+                <h1 className={classes.h1}><TbHeartbeat/> Gym Training Planner</h1>
             </IconContext.Provider>
     );
 };
