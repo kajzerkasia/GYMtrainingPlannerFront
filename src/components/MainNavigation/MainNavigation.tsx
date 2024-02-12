d4b162849239f276f7f32fcf1c9900185eb84879
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-12 17:20:01 +0100

    Added hamburger-menu for mobile views.

--- src/components/MainNavigation/MainNavigation.tsx
+++ src/components/MainNavigation/MainNavigation.tsx
@@ -10,7 +10,8 @@ import {UserEntity} from "../../constants/types";
 const MainNavigation = () => {
     const token: any = useRouteLoaderData('root');
     const dispatch = useDispatch();
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useState(false);
 
     useEffect(() => {
         if (token) {
@@ -28,19 +29,26 @@ const MainNavigation = () => {
         }
     }
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    }
+
+    const toggleHamburger = () => {
+        setIsHamburgerMenuOpen(!isHamburgerMenuOpen);
     }
 
 
     return (
         <header className={classes.header}>
-            <Link to="/">
-                <Logo/>
-            </Link>
+            <div className={classes.logo_and_hamburger_container}>
+                <Link to="/">
+                    <Logo/>
+                </Link>
+                <div className={`${classes.hamburgerIcon} ${isHamburgerMenuOpen ? classes.hamburgerOpen : ''}`} onClick={toggleHamburger}></div>
+            </div>
             <nav>
-                <ul className={classes.navList}>
-                    <li>
+                <ul className={`${classes.navList} ${isHamburgerMenuOpen ? classes.mobileMenu : ''}`}>
+                    <li className={isHamburgerMenuOpen ? classes.dropdownOpen : classes.dropdownClose}>
                         <NavLink to='/' className={({isActive}) =>
                             isActive ? `${classes.active} ${classes.buttonWithAnimation}` : classes.buttonWithAnimation
                         }>
@@ -49,7 +57,7 @@ const MainNavigation = () => {
                             </button>
                         </NavLink>
                     </li>
-                    <li>
+                    <li className={isHamburgerMenuOpen ? classes.dropdownOpen : classes.dropdownClose}>
                         <NavLink to={`/list/${userId}`} className={({isActive}) =>
                             isActive ? `${classes.active} ${classes.buttonWithAnimation}` : classes.buttonWithAnimation
                         }>
@@ -58,7 +66,7 @@ const MainNavigation = () => {
                             </button>
                         </NavLink>
                     </li>
-                    <li>
+                    <li className={isHamburgerMenuOpen ? classes.dropdownOpen : classes.dropdownClose}>
                         <NavLink to={`/calendar/${userId}`} className={({isActive}) =>
                             isActive ? `${classes.active} ${classes.buttonWithAnimation}` : classes.buttonWithAnimation
                         }>
@@ -81,11 +89,11 @@ const MainNavigation = () => {
                         </li>
                     )}
                     {token && (
-                        <li>
-                            <button onClick={toggleMenu} className={classes.button_avatar}>
+                        <li className={isHamburgerMenuOpen ? classes.hamburgerOpen : classes.hamburgerClose}>
+                            <button onClick={toggleDropdown} className={classes.button_avatar}>
                                 <img src="/assets/images/avatar_man.png" alt="" className={classes.avatar_img}/>
                             </button>
-                            {menuOpen && (
+                            {isDropdownOpen && (
                                 <div className={classes.dropdownMenu}>
                                     <Form action="/logout" method="post">
                                         <button>
