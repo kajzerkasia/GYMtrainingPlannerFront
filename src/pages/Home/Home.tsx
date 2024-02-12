367d7efe5221b5ae594cb9c08510a7d1552a0f80
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-13 00:18:16 +0100

    Created additional button for mobile only view.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ImagesSlideshow from "../../components/ImagesSlideshow/ImagesSlideshow";
 import {useSelector} from "react-redux";
 import {RootState} from "../../store";
-import {useRouteLoaderData} from "react-router-dom";
+import {NavLink, useRouteLoaderData} from "react-router-dom";
 import classes from './Home.module.css';
 import {UserEntity} from "../../constants/types";
 
@@ -12,20 +12,32 @@ const Home = () => {
     const {users} = useSelector((state: RootState) => state.items);
     const usersList = users as unknown as UserEntity;
     const userName = usersList?.name;
+    const userId = usersList?.id;
 
     return (
-        <div className={classes.div_home_container}>
-            <div className={classes.home_card}>
-                <header className={classes.home_header}>
-                    <h1 className={classes.home_h1}>Witaj{(token && userName) ? ` ${userName}!` : ' w aplikacji Gym Training Planner'}</h1>
-                    {token ? (
-                        <p>Już teraz zarządzaj swoimi planami treningowymi</p>
-                    ) : (
-                        <p>Zaloguj się lub zarejestruj aby zarządzać planami treningowymi</p>
-                    )}
-                </header>
-                <div className={classes.slideshow}>
-                    <ImagesSlideshow/>
+        <div className={classes.div_container}>
+            <div className={classes.button_mobile_only}>
+                {token && (
+                    <NavLink to={`/list/${userId}`}>
+                        <button className={classes.plans_btn}>
+                            Plany treningowe
+                        </button>
+                    </NavLink>
+                )}
+            </div>
+            <div className={classes.div_home_container}>
+                <div className={classes.home_card}>
+                    <header className={classes.home_header}>
+                        <h1 className={classes.home_h1}>Witaj{(token && userName) ? ` ${userName}!` : ' w aplikacji Gym Training Planner'}</h1>
+                        {token ? (
+                            <p>Już teraz zarządzaj swoimi planami treningowymi</p>
+                        ) : (
+                            <p>Zaloguj się lub zarejestruj aby zarządzać planami treningowymi</p>
+                        )}
+                    </header>
+                    <div className={classes.slideshow}>
+                        <ImagesSlideshow/>
+                    </div>
                 </div>
             </div>
         </div>
