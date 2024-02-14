21087aa784e1794161266f30178c5d5d5a06f833
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-14 21:13:00 +0100

    Created AvatarPicker.tsx

---
+++ src/components/AvatarPicker/AvatarPicker.tsx
@@ -0,0 +1,29 @@
+import classes from './AvatarPicker.module.css';
+
+interface AvatarPickerProps {
+    images: Array<{ path: string; caption: string }>;
+    selectedImage: string;
+    onSelect: (imagePath: string) => void;
+}
+
+export default function AvatarPicker({ images, selectedImage, onSelect }: AvatarPickerProps) {
+    return (
+        <div className={classes.image_picker}>
+            <p>*Avatar</p>
+            <ul>
+                {images.map((image: any) => (
+                    <li
+                        key={image.path}
+                        onClick={() => onSelect(image.path)}
+                        className={selectedImage === image.path ? `${classes.selected}` : undefined}
+                    >
+                        <img
+                            src={`http://localhost:3001/public/${image.path}`}
+                            alt={image.caption}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
