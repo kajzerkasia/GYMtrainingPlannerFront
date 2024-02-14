7a156fc73bd14b25d06782a5a777003c61de9ed0
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-14 21:14:21 +0100

    Added dynamic path for img.

--- src/components/AvatarButton/AvatarButton.tsx
+++ src/components/AvatarButton/AvatarButton.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Form} from "react-router-dom";
 import classes from './AvatarButton.module.css';
+import {useSelector} from "react-redux";
+import {RootState} from "../../store";
+import {UserEntity} from "../../constants/types";
 
 interface AvatarButtonProps {
     onClick: () => void;
@@ -8,11 +11,16 @@ interface AvatarButtonProps {
     isHamburgerMenuOpen: boolean;
 }
 
+
 const AvatarButton = ({ onClick, isDropdownOpen, isHamburgerMenuOpen }: AvatarButtonProps) => {
+    const { users } = useSelector((state: RootState) => state.items);
+    const usersList = users as unknown as UserEntity;
+    const userImage = usersList?.image;
+
     return (
         <>
             <button onClick={onClick} className={classes.button_avatar}>
-                <img src="/assets/images/avatar_man.png" alt="Avatar" className={classes.avatar_img} />
+                <img src={`/assets/images/${userImage}`} alt="Avatar" className={classes.avatar_img} />
             </button>
             {isDropdownOpen && (
                 <div className={`${classes.dropdownMenu} ${isDropdownOpen ? classes.open : ''}`}>
