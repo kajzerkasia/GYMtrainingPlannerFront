c83a2a7c87b10179b547130bf72005caa7552e1a
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-26 20:59:31 +0100

    Added IconProvider and Button components.

--- src/components/Calendar/CalendarInstruction/CalendarInstruction.tsx
+++ src/components/Calendar/CalendarInstruction/CalendarInstruction.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import CalendarInstructionList from "../CalendarInstructionList/CalendarInstructionList";
 import {ADD_TRAINING_INSTRUCTION, DELETE_TRAINING_INSTRUCTION, EDIT_TRAINING_INSTRUCTION} from "../../../constants/calendarInstruction";
 import {TbPencil, TbPlus, TbX} from "react-icons/tb";
-import {IconContext} from "react-icons";
 import classes from './CalendarInstruction.module.css';
+import IconProvider from "../../IconProvider/IconProvider";
+import Button from "../../Button/Button";
 
 interface CalendarInstructionProps {
     toggleAccordion: () => void;
@@ -14,31 +15,37 @@ const CalendarInstruction = ({toggleAccordion}: CalendarInstructionProps) => {
         <div className={classes.div_container}>
             <div className={classes.header}>
                 <h1 className={classes.h1}>Jak to działa?</h1>
-                <IconContext.Provider value={{className: 'react-icons'}}>
-                    <button
+                <IconProvider>
+                    <Button
                         className={classes.button}
                         onClick={toggleAccordion}>
                         <TbX/>
-                    </button>
-                </IconContext.Provider>
+                    </Button>
+                </IconProvider>
             </div>
             <CalendarInstructionList
                 array={ADD_TRAINING_INSTRUCTION}
                 title="Jak dodać trening do kalendarza?"
             >
-                <TbPlus/>
+                <IconProvider>
+                    <TbPlus/>
+                </IconProvider>
             </CalendarInstructionList>
             <CalendarInstructionList
                 array={EDIT_TRAINING_INSTRUCTION}
                 title="Jak edytować dodany trening w kalendarzu?"
             >
-                <TbPencil/>
+                <IconProvider>
+                    <TbPencil/>
+                </IconProvider>
             </CalendarInstructionList>
             <CalendarInstructionList
                 array={DELETE_TRAINING_INSTRUCTION}
                 title="Jak usunąć dany trening z kalendarza?"
             >
-                <TbX/>
+                <IconProvider>
+                    <TbX/>
+                </IconProvider>
             </CalendarInstructionList>
         </div>
     );
