f127f82d8a666bfd0156ec2f298f0eb8a8e82bfc
Kasia <kajzerkatarzyna1@gmail.com>
2024-02-26 22:59:18 +0100

    Expanded the component.

--- src/pages/Calendar/CalendarEvents.tsx
+++ src/pages/Calendar/CalendarEvents.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootState} from "../../store";
+import {fetchTrainingsData} from "../../store/actions/calendar/fetching-action";
+import {useParams} from "react-router-dom";
 
 const CalendarEvents = () => {
+    const params = useParams();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                (dispatch as AppDispatch)(fetchTrainingsData(params));
+            } catch (error) {
+                console.error("Wystąpił błąd podczas pobierania danych o treningach:", error);
+            }
+        };
+
+        fetchData();
+    }, [dispatch]);
+
+    const {
+        events,
+    } = useSelector((state: RootState) => state.calendar);
+
+
     return (
         <div>
-            EVENTS
+            {events.map((event) => (
+                <div key={event.id}>
+                    <p>{event.planName}</p>
+                    <p>{event.partName}</p>
+                    <p>{event.startTime} - {event.endTime}</p>
+                </div>
+            ))}
         </div>
     );
 };
