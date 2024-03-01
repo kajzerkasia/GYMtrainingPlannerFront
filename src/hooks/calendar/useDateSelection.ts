e53ec9f8b46f8eb33f88f9eb6abfbe1911fdfe92
Kasia <kajzerkatarzyna1@gmail.com>
2024-03-01 15:40:34 +0100

    Added saving selectedDate in localStorage.

--- src/hooks/calendar/useDateSelection.ts
+++ src/hooks/calendar/useDateSelection.ts
@@ -1,39 +1,33 @@
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../store";
 import {calendarsActions} from "../../store/features/calendar/calendar-slice";
-import moment from "moment";
 import {useNavigate} from "react-router-dom";
 import {UserEntity} from "../../constants/types";
+import {useState} from "react";
 
 export const UseDateSelection = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const {selectedDate} = useSelector((state: RootState) => state.calendar);
+    // const {selectedDate} = useSelector((state: RootState) => state.calendar);
 
-    const { users } = useSelector((state: RootState) => state.items);
+    const {users} = useSelector((state: RootState) => state.items);
     const usersList = users as unknown as UserEntity;
     const userId = usersList?.id;
 
-    const unselectDate = () => {
-        dispatch(calendarsActions.selectDate(null));
-    };
+    const initialSelectedDate = localStorage.getItem('selectedDate');
+    const [selectedDate, setSelectedDate] = useState<number | null>(initialSelectedDate ? parseInt(initialSelectedDate, 10) : null);
 
     const handleSelect = ({start}: { start: Date }) => {
 
         const startTimestamp = start.getTime();
+        console.log(startTimestamp)
 
-        const isSameDate = selectedDate ? moment(selectedDate).isSame(start, 'day') : false;
-
-        if (isSameDate) {
-            dispatch(calendarsActions.selectDate(null));
-            dispatch(calendarsActions.toggleAddTrainingToCalendar(false));
-        } else {
-            dispatch(calendarsActions.selectDate(startTimestamp));
-            dispatch(calendarsActions.toggleAddTrainingToCalendar(true));
-        }
+        dispatch(calendarsActions.selectDate(startTimestamp));
+        setSelectedDate(startTimestamp);
+        localStorage.setItem('selectedDate', startTimestamp.toString());
 
         navigate(`/calendar/${userId}/trainings`);
     };
 
-    return { selectedDate, unselectDate, handleSelect };
+    return {handleSelect, selectedDate};
 };
