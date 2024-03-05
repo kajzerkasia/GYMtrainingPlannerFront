cc912c52b9accfe09cb11ce7312e00e02e69d604
Kasia <kajzerkatarzyna1@gmail.com>
2024-03-05 12:55:12 +0100

    Formatted code.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,7 +6,8 @@ import logoImg from '../../images/logo_img.png';
 const Logo = () => {
     return (
         <div className={classes.logo}>
-            <img src={logoImg} alt=""/><h1>Gym Training Planner</h1>
+            <img src={logoImg} alt=""/>
+            <h1>Gym Training Planner</h1>
         </div>
     );
 };
