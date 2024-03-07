852138757a1bbf8dcfa0d62fe46f6580618eb021
Kasia <kajzerkatarzyna1@gmail.com>
2024-03-07 20:47:41 +0100

    Updated import, formatted code.

--- src/pages/Calendar/CalendarEventsHeader.tsx
+++ src/pages/Calendar/CalendarEventsHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from "./CalendarEvents.module.css";
+import classes from "./CalendarEventsHeader.module.css";
 import img from "../../images/resized-hantel.png";
 import CalendarEventsButtons from "./CalendarEventsButtons";
 
@@ -9,10 +9,19 @@ interface CalendarEventsHeaderProps {
 
 const CalendarEventsHeader = ({chosenDate}: CalendarEventsHeaderProps) => {
     return (
-        <div className={classes.header_container}>
-            <img src={img} alt=""/>
-            <header className={classes.header}>
-                <h1 className={classes.h1}>Treningi zaplanowane na {chosenDate}</h1>
+        <div
+            className={classes.header_container}
+        >
+            <img
+                src={img}
+                alt=""
+            />
+            <header>
+                <h1
+                    className={classes.h1}
+                >
+                    Treningi zaplanowane na {chosenDate}
+                </h1>
                 <CalendarEventsButtons/>
             </header>
         </div>
